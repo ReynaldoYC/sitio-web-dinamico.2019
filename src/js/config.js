a17069372d13d594069b4c902ad2ab9cb2cc2ad6
Reynaldo Yacila <yacila1717@gmail.com>
2019-03-15 23:25:01 -0500

    commit inicial

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+
+const config = {
+    piKey: "AIzaSyCnNOZxIU6H6j4_CiU0xLd5JW68ulnXsR8",
+    authDomain: "edproyect-c2892.firebaseapp.com",
+    databaseURL: "https://edproyect-c2892.firebaseio.com",
+    projectId: "edproyect-c2892",
+    storageBucket: "edproyect-c2892.appspot.com",
+    messagingSenderId: "302633112624"
+}
+const init = () => firebase.initializeApp(config);
